837bd2c42175b1a8e48d05a90f809e879aca4597
Centripio <code@centripio.io>
2020-05-25 15:31:35 -0700

    [Discovery] Creando un descubridor de servicios (Service Discovery).

---
+++ src/main/java/com/geekshirt/discovery/GeekshirtDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.geekshirt.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class GeekshirtDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekshirtDiscoveryServiceApplication.class, args);
+	}
+
+}
